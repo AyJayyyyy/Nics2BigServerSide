085ae475fd696a57ff20bb900237c25dbf30bd58
Jorel I. Agustin <106132941+AyJayyyyy@users.noreply.github.com>
2023-07-15 21:25:44 +0800

    ui dashboard add

--- nics2bigserver/routes/web.php
+++ nics2bigserver/routes/web.php
@@ -16,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('admin.dashboard');
 })->name('home');
+
 Route::get('/login', [AuthManager::class, 'login'])->name('login');
 Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post');
 Route::get('/registration', [AuthManager::class, 'registration'])->name('registration');
